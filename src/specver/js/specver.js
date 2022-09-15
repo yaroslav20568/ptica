ee6296f8c4997edfa75be9431d7ed8cdd2ea34a8
oleg_nbd <oleg@nbd.by>
2022-09-15 11:00:48 +0300

    fixed eye widget

---
+++ src/specver/js/specver.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+function loadform(){
+  $( "<button style='float:right;width: 100%;font-size:24px;padding:10px 0;'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
+$( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" );  
+}    
+
+$('.spec').click(function (e) {
+
+if (!$.cookie('openspec'))
+{
+$.cookie('openspec',1, { path: '/' });
+$("body *").css("background", "#fff");
+$("body *").css("color", "#000");
+$("img").css("-webkit-filter", "grayscale(100%)");
+loadform();
+}});
+
+if ($.cookie('openspec')==1)
+loadform();
+
+
+
+});
